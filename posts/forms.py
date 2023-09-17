f4cf3bdad9553095baf3f24c5f51ac6ef53a23ef
rybolev <dark18@yandex.ru>
2023-09-17 19:44:40 +0500

    выбор категорий при создании поста

--- posts/forms.py
+++ posts/forms.py
@@ -1,21 +1,31 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from accounts.models import Author
 from .models import Post, Category, Comment
 
 
 class NewsForm(forms.ModelForm):
     header = forms.CharField(label='Header', max_length=200)
     text = forms.TextInput()
+    categories = forms.ModelMultipleChoiceField(
+        queryset=Category.objects.all(),
+        label='Categories',
+        widget=forms.CheckboxSelectMultiple(),
+    )
 
     class Meta:
         model = Post
         # fields = '__all__'
         fields = [
             'header',
+            'categories',
             'text',
         ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['categories'].widget.choices = [(category.id, category.name) for category in Category.objects.all()]
+        self.fields['categories'].label_from_instance = lambda obj: obj.name
 
     def clean(self):
         perm_len = 200
